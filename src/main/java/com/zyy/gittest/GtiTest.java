462fcf69c8d9171d741ff6d9654f97efdba25218
trol <zyydy3699@outlook.com>
2025-09-22 17:45:19 +0800

    hello git_idea 2

--- src/main/java/com/zyy/gittest/GtiTest.java
+++ src/main/java/com/zyy/gittest/GtiTest.java
@@ -3,5 +3,6 @@ package com.zyy.gittest;
 public class GtiTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git second!");
     }
 }
